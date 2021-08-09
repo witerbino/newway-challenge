1986460f9c6fe127d900067dcee46090d449b090
Wallace Viterbino <witerbino@gmail.com>
2021-08-08 23:43:36 -0300

    feat: add config server

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const App = require('./src/app');
 const server = new App().init();
 
+require('./src/database');
+
 server.listen(process.env.APP_PORT, () => {
   console.log(`App started in port ${process.env.APP_PORT}`);
 });
